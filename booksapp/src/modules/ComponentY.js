349586713d77fce872fe7b1f7a7dc92d02221a50
Karol Andracki <andracki@gmail.com>
2019-11-14 01:11:43 +0100

    componentY made responsive

--- booksapp/src/modules/ComponentY.js
+++ booksapp/src/modules/ComponentY.js
@@ -2,10 +2,22 @@ import React from 'react';
 import '../modules/Component.module.css';
 
 function ComponentY() {
+  const mystyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column"
+  }
+
+  const imag = {
+    height: "60vh",
+    maxWidth: "60%"
+    
+  }
   return (
-    <div className="component">
+    <div style={mystyle} >
       <p>cos tam w zaleznosci jak sie zmieni</p>
-      <img src="http://via.placeholder.com/600x600"></img>
+      <div style={imag}><img src="http://via.placeholder.com/600x600" width="100%"></img></div>
     </div>
   );
 }
