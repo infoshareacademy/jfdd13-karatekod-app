21ab1b06dd79b310f25506c5170a4184ccca43a1
Karol Andracki <andracki@gmail.com>
2019-11-14 02:41:06 +0100

    layout to fix

--- booksapp/src/App.js
+++ booksapp/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import  Aside from '../src/modules/Aside';
 import Footer from '../src/modules/Footer';
 import Nav from '../src/modules/Nav'
-import ComponentX from '../src/modules/ComponentX'
+import ComponentY from '../src/modules/ComponentY'
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
@@ -14,7 +14,7 @@ function App() {
         
         <Nav />
         <Aside />
-        <ComponentX />
+        <ComponentY />
         
         <Footer />
         
---
+++ booksapp/src/modules/ComponentY.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+function ComponentY() {
+  const mystyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    
+
+    
+  }
+
+  const imag = {
+    
+    
+    maxWidth: "100%"
+    
+  }
+  return (
+    <div style={mystyle} >
+      <p>cos tam w zaleznosci jak sie zmieni</p>
+      <div style={imag}><img src="http://via.placeholder.com/600x600" width="100%"></img></div>
+    </div>
+  );
+}
+
+export default ComponentY;
