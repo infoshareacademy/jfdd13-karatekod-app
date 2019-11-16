56f78b79868216d9b1b5655840308ac8788148a8
Karol Andracki <andracki@gmail.com>
2019-11-16 23:05:51 +0100

    imports of main components and modules to app.js

--- booksapp/src/App.js
+++ booksapp/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import  Aside from '../src/modules/Aside';
 import Footer from '../src/modules/Footer';
-import Nav from '../src/modules/Nav'
-import ComponentX from '../src/modules/ComponentX'
-import ComponentY from '../src/modules/ComponentY'
-import ComponentZ from '../src/modules/ComponentZ'
+import Nav from '../src/modules/Nav';
+import ComponentX from '../src/modules/ComponentX';
+import ComponentY from '../src/modules/ComponentY';
+import ComponentZ from '../src/modules/ComponentZ';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import "bootstrap/dist/css/bootstrap/min.css";
+import Home from './pages/HomePage';
+import SingleProduct from './pages/SingleProductPage';
+import Products from './pages/ProductsPage'
+import About from './pages/AboutPage'
+import Default from './pages/DefaultPage'
+
+
 
 
 function App() {
@@ -20,6 +28,12 @@ function App() {
           <Route path="/x" exact component={ComponentX} />
           <Route path="/y" exact component={ComponentY} />
           <Route path="/z" exact component={ComponentZ} />
+          <Route path="/about" exact component={About} />
+          <Route path="/home" exact component={Home} />
+          <Route path="/product" exact component={SingleProduct} />
+          <Route path="/default" exact component={Default} />
+          <Route path="/products" exact component={Products} />
+          
         </Switch>
         <div className="clear"></div>
         <Footer />
--- booksapp/src/modules/Nav.js
+++ booksapp/src/modules/Nav.js
@@ -7,33 +7,20 @@ import TextField from '@material-ui/core/TextField'
 
 
 
-export class Nav extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    )
-  }
+
+function Nav() {
+  return (
+    <nav>
+      <img src="http://via.placeholder.com/50x50"></img>
+      <form>
+        <SearchText />
+        <SearchChBox />
+        <SearchRange />
+        <button>Search</button>
+      </form>
+      <h3>login/register</h3>
+    </nav>
+  );
 }
 
-export default Nav
-
-
-
-// function Nav() {
-//   return (
-//     <nav>
-//       <img src="http://via.placeholder.com/50x50"></img>
-//       <form>
-//         <SearchText />
-//         <SearchChBox />
-//         <SearchRange />
-//         <button>Search</button>
-//       </form>
-//       <h3>login/register</h3>
-//     </nav>
-//   );
-// }
-
-// export default Nav;
+export default Nav;
