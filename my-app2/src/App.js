e6cc62aeb4e5b981652e4f9bc90736e360ed64da
Karolina Michalak <karolina0michalak@gmail.com>
2019-11-16 11:00:57 +0100

    route path added

--- my-app2/src/App.js
+++ my-app2/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom'
+
+const Home = () => {
+  return (
+  <h1>Strona startowa</h1>
+  )
+}
+
+const News = () => {
+  return  (
+  <h1>News</h1>
+  )
+}
+
+const Contact = () => {
+  return (
+  <h1>Contact us</h1>
+  )
+}
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <div>
         <heder>
           <nav>
             <li><Link to="/">Start</Link></li>
-            <li><Link to="/news">Aktualności</Link></li>
-            <li><Link to="/contact">Kontakt</Link></li>
+            <li><Link to="/news">News</Link></li>
+            <li><Link to="/contact">Contact us</Link></li>
           </nav>
 
           <section>
-            oooo
+          <Route path="/" exact component={Home}/>
+          <Route path="/news" component={News}/>
+          <Route path="/contact" component={Contact}/>
           </section>
 
 
         </heder>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 }
 
