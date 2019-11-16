14173793682d3a36d4f3f0e4725e6cd8d47db046
Karolina Michalak <karolina0michalak@gmail.com>
2019-11-16 12:13:51 +0100

    new app docs created

---
+++ my-app2/src/layouts/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        SPA
+
+      </div>
+      </Router>
+  );
+}
+
+export default App;
