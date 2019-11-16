5f0979dfdb240aad45f013115400f01240ed83fc
Karol Andracki <andracki@gmail.com>
2019-11-16 22:24:11 +0100

    books imgs, empty components: about, default

--- booksapp/src/modules/Nav.js
+++ booksapp/src/modules/Nav.js
@@ -1,40 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Nav.module.css';
-import SearchText from "./SearchText";
-import SearchChBox from "./SearchChBox"
-import SearchRange from "./SearchRange"
-import { anyTypeAnnotation } from '@babel/types';
+import TextField from '@material-ui/core/TextField'
 
 
 
-// class BooksProvider extends Component {
-//   state = {
-//     search: '',  //autor lub tytul
-//     radius: 0,
-//     genere: 'any',
 
 
 
-//   }
+export class Nav extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Nav
+
+
+
+// function Nav() {
+//   return (
+//     <nav>
+//       <img src="http://via.placeholder.com/50x50"></img>
+//       <form>
+//         <SearchText />
+//         <SearchChBox />
+//         <SearchRange />
+//         <button>Search</button>
+//       </form>
+//       <h3>login/register</h3>
+//     </nav>
+//   );
 // }
 
-handleChange = (event) => {
-  
-}
-
-function Nav() {
-  return (
-    <nav>
-      <img src="http://via.placeholder.com/50x50"></img>
-      <form>
-        <SearchText />
-        <SearchChBox />
-        <SearchRange />
-        <button>Search</button>
-      </form>
-      <h3>login/register</h3>
-    </nav>
-  );
-}
-
-export default Nav;
+// export default Nav;
