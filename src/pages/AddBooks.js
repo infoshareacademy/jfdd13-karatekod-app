a524e8f327940f4de624e36ed66f7d99a93ae79c
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-18 23:18:39 +0100

    Adds book to localstorage with data from inputs

--- src/pages/AddBooks.js
+++ src/pages/AddBooks.js
@@ -1,58 +1,120 @@
 import React from 'react';
-import styles from "../styles/AddBooks.module.css";
-import {books} from '../pages/BooksListPage'
+import styles from "../styles/AddBooks.module.css"; // imports css styles
+import uuid from "uuid";
+import {booksList} from '../pages/BooksListPage' // imports booksList from the bookListPage.js
 
-
-
-class AddBooks extends React.Component{
+class AddBooks extends React.Component{ // AddBooks component
     constructor(props){
         super(props)
         this.state = {
-            booksList:books
+            booksList:booksList,
+            newTitle: "",
+            newAutor: "",
+            newType: "",
+            newImageUrl: "",
+            newCondition: "",
         }
     }
 
-    addBook = () => {
+    addBook = () => {   // method for adding new books (now it just adds the same xxx book... gotta get the values from inputs somehow)
         const newBook = {
-          id: 777,
-          title: 'xxx',
-          author: 'yyy',
-          genre: 'zzz',
-          cover: 'vvv'
+          id: uuid.v4(),
+          title: this.state.newTitle,
+          autor: this.state.newAutor,
+          type: this.state.newType,
+          imageUrl: "http://placekitten.com/140/190",
+          condition: this.state.newCondition
     
         }
         this.setState({
-            booksList:[...this.state.booksList, newBook]
+            booksList:[...this.state.booksList, newBook],
+            newTitle: "",
+            newAutor: "",
+            newType: "",
+            newImageUrl: "",
+            newCondition: "",    // adds the new book to the bookList state
         })
+
+        
+
      };
+
+     handleTitle = newValue =>{
+         if (newValue.length>40){
+             return
+         }
+         this.setState({
+             newTitle:newValue
+         })
+     }
+     handleAutor = newValue =>{
+        if (newValue.length>40){
+            return
+        }
+        this.setState({
+            newAutor:newValue
+        })
+    }
+    handleType= newValue =>{
+        if (newValue.length>40){
+            return
+        }
+        this.setState({
+            newType:newValue
+        })
+    }
+    handleImageUrl = newValue =>{
+        
+        this.setState({
+            newImageUrl:newValue
+        })
+    }
+    handleCondition = newValue =>{
+        
+        this.setState({
+            newCondition:newValue
+        })
+    }
+
+     componentDidUpdate() {
+        localStorage.setItem("bookslist", JSON.stringify(this.state.booksList));    // local storage updates whenever something changes in this component
+      }
 
      render() {
         return (
             <div className={styles.wrap}>
                  <form className={styles.form}>
       <label className={styles.label}>Title:</label>
-      <input className={styles.input} type="text" name="title" placeholder="Insert title name here"/>
-      
+      <input className={styles.input} type="text" name="title" placeholder="Insert title name here" value={this.newTitle} onChange={event => {
+        this.handleTitle(event.target.value);
+      }}/>
+
       <label className={styles.label}>Author:</label>
-      <input className={styles.input} type="text" name="author" placeholder="Insert author name here"/>
-    
+      <input className={styles.input} type="text" name="autor" placeholder="Insert author name here" value={this.newAutor} onChange={event => {
+        this.handleAutor(event.target.value);}}/>
+
       <label className={styles.label}>Genre:</label>
-      <input className={styles.input} type="text" name="genre" placeholder="Insert genre name here"/>
-    
+      <input className={styles.input} type="text" name="type" placeholder="Insert genre name here" value={this.newType} onChange={event => {
+        this.handleType(event.target.value);}}/>
+
       <label className={styles.label}>Cover photo:</label>
-      <input className={styles.input} type="text" name="cover photo" placeholder="Insert cover photo here"/>
+      <input className={styles.input} type="text" name="imageUrl" placeholder="URL, ex. http://placekitten.com/140/190 " value={this.newImageUrl} onChange={event => {
+        this.handleImageUrl(event.target.value);}} />
+
+      <label className={styles.label}>Condition:</label>
+      <input className={styles.input} type="text" name="condition" placeholder="What is the condition of your book?" value={this.newCondition} onChange={event => {
+        this.handleCondition(event.target.value);}}/>
     
       </form>
     <button className={styles.button} onClick={()=>{
                 this.addBook();
+                alert(`You added "${this.state.newTitle}`)
                 console.log('added')
                 console.log(this.state)
                 
             }}>
-                Add this book 
-            </button>
-            <ul>{this.state.booksList.map(book => <li>{book.title}</li>)}</ul>
-           
+                ADD TO BOOKSWAPP
+            </button>           
             </div>
         )
      }
--- src/pages/BooksListPage.js
+++ src/pages/BooksListPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Book from '../components/Book.js';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import uuid from "uuid";
 import styles from "../styles/Books.module.css";
 
-export let books= (() => {
+export let books = (() => {
     let bookslist = localStorage.getItem("bookslist");
     if (!bookslist) {
         bookslist = [
@@ -13,94 +13,108 @@ export let books= (() => {
                 title: "Metro 2033",
                 autor: "Dmitry Glukhovsky",
                 type: "fantasy",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 5
             },
             {
                 id: uuid.v4(),
                 title: "Harry Potter",
                 autor: "J. K. Rowling",
                 type: "fantasy",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 5
             },
             {
                 id: uuid.v4(),
                 title: "Dziady cz. II",
                 autor: "Adam Mickiewicz",
                 type: "poetry",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 4
             },
             {
                 id: uuid.v4(),
                 title: "Zielona Mila",
                 autor: "Stephen King",
                 type: "horror",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 7
             },
             {
                 id: uuid.v4(),
                 title: "Wstrząs",
                 autor: "Robin Cook",
                 type: "thriller",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 10
             },
             {
                 id: uuid.v4(),
                 title: "Dziady cz. II",
                 autor: "Adam Mickiewicz",
                 type: "poetry",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 1
             },
             {
                 id: uuid.v4(),
                 title: "Dziady cz. II",
                 autor: "Adam Mickiewicz",
                 type: "poetry",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 8
             },
             {
                 id: uuid.v4(),
                 title: "Zielona Mila",
                 autor: "Stephen King",
                 type: "horror",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 4
             },
             {
                 id: uuid.v4(),
                 title: "Wstrząs",
                 autor: "Robin Cook",
                 type: "thriller",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 7
             },
             {
                 id: uuid.v4(),
                 title: "Dziady cz. II",
                 autor: "Adam Mickiewicz",
                 type: "poetry",
-                imageUrl: "http://placekitten.com/140/190"
+                imageUrl: "http://placekitten.com/140/190",
+                condition: 6
             }
         ];
         localStorage.setItem('bookslist', JSON.stringify(bookslist));
     }
-    return JSON.parse(JSON.stringify(bookslist));
-    
+    return JSON.parse(localStorage.getItem("bookslist"))
+
+
 })()
 
 
 
 const BooksListPage = () => {
-    console.log(localStorage.getItem("bookslist"))
-    console.log(typeof localStorage.getItem("bookslist"))
     const booksList = JSON.parse(localStorage.getItem("bookslist")).map(book => (
+
         <Link to={`/book/${book.id}`}><Book key={book.id} {...book} /></Link>
-      ))
-  
-  
-      return (
-          <div className={styles.books}>
-              <h2>List of books</h2>
-              <div className={styles.list}>{booksList}</div>
-          </div>
-      );
-  }
-  
+    ))
+
+
+    return (
+        <div className={styles.books}>
+            <h2>List of books</h2>
+            <div className={styles.list}>{booksList}</div>
+        </div>
+    );
+}
+
+
+export let booksList = JSON.parse(localStorage.getItem("bookslist"))
+
 export default BooksListPage
+
