b8e5830b3ba9a455dae79656e5df1c3bc9bfc552
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-19 14:44:06 +0100

    inputs cleared after Adding a book

--- src/pages/AddBooks.js
+++ src/pages/AddBooks.js
@@ -2,21 +2,24 @@ import React from 'react';
 import styles from "../styles/AddBooks.module.css"; // imports css styles
 import uuid from "uuid";
 import {booksList} from '../pages/BooksListPage' // imports booksList from the bookListPage.js
-
+const initialState = {
+    newTitle: "",
+    newAutor: "",
+    newType: "",
+    newImageUrl: "",
+    newCondition: "",
+}
 class AddBooks extends React.Component{ // AddBooks component
     constructor(props){
         super(props)
         this.state = {
             booksList:booksList,
-            newTitle: "",
-            newAutor: "",
-            newType: "",
-            newImageUrl: "",
-            newCondition: "",
+            ...initialState
         }
     }
 
-    addBook = () => {   // method for adding new books (now it just adds the same xxx book... gotta get the values from inputs somehow)
+    addBook = (e) => {   // method for adding new books (now it just adds the same xxx book... gotta get the values from inputs somehow)
+        e.preventDefault()
         const newBook = {
           id: uuid.v4(),
           title: this.state.newTitle,
@@ -28,15 +31,8 @@ class AddBooks extends React.Component{ // AddBooks component
         }
         this.setState({
             booksList:[...this.state.booksList, newBook],
-            newTitle: "",
-            newAutor: "",
-            newType: "",
-            newImageUrl: "",
-            newCondition: "",    // adds the new book to the bookList state
+            ...initialState
         })
-
-        
-
      };
 
      handleTitle = newValue =>{
@@ -81,40 +77,44 @@ class AddBooks extends React.Component{ // AddBooks component
       }
 
      render() {
+        const { newTitle, newAutor, newType, newImageUrl, newCondition} = this.state;
+
         return (
             <div className={styles.wrap}>
                  <form className={styles.form}>
       <label className={styles.label}>Title:</label>
-      <input className={styles.input} type="text" name="title" placeholder="Insert title name here" value={this.newTitle} onChange={event => {
+      <input className={styles.input} type="text" name="title" placeholder="Insert title name here" value={newTitle} onChange={event => {
         this.handleTitle(event.target.value);
       }}/>
 
       <label className={styles.label}>Author:</label>
-      <input className={styles.input} type="text" name="autor" placeholder="Insert author name here" value={this.newAutor} onChange={event => {
+      <input className={styles.input} type="text" name="autor" placeholder="Insert author name here" value={newAutor} onChange={event => {
         this.handleAutor(event.target.value);}}/>
 
       <label className={styles.label}>Genre:</label>
-      <input className={styles.input} type="text" name="type" placeholder="Insert genre name here" value={this.newType} onChange={event => {
+      <input className={styles.input} type="text" name="type" placeholder="Insert genre name here" value={newType} onChange={event => {
         this.handleType(event.target.value);}}/>
 
       <label className={styles.label}>Cover photo:</label>
-      <input className={styles.input} type="text" name="imageUrl" placeholder="URL, ex. http://placekitten.com/140/190 " value={this.newImageUrl} onChange={event => {
+      <input className={styles.input} type="text" name="imageUrl" placeholder="URL, ex. http://placekitten.com/140/190 " value={newImageUrl} onChange={event => {
         this.handleImageUrl(event.target.value);}} />
 
       <label className={styles.label}>Condition:</label>
-      <input className={styles.input} type="text" name="condition" placeholder="What is the condition of your book?" value={this.newCondition} onChange={event => {
+      <input className={styles.input} type="text" name="condition" placeholder="What is the condition of your book?" value={newCondition} onChange={event => {
         this.handleCondition(event.target.value);}}/>
     
-      </form>
-    <button className={styles.button} onClick={()=>{
-                this.addBook();
-                alert(`You added "${this.state.newTitle}`)
+    <button className={styles.button} onClick={(e)=>{
+                this.addBook(e);
+                // alert(`You added "${this.state.newTitle}`)
                 console.log('added')
                 console.log(this.state)
                 
             }}>
                 ADD TO BOOKSWAPP
-            </button>           
+            </button>   
+
+      </form>
+        
             </div>
         )
      }
