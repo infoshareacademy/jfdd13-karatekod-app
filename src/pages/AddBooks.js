72258d2145544f72e3d9c371f362c70d768cbe45
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-19 16:04:59 +0100

    validation of inputs - highlights red when empty

--- src/pages/AddBooks.js
+++ src/pages/AddBooks.js
@@ -9,12 +9,20 @@ const initialState = {
     newImageUrl: "",
     newCondition: "",
 }
+
+
 class AddBooks extends React.Component{ // AddBooks component
     constructor(props){
         super(props)
         this.state = {
             booksList:booksList,
-            ...initialState
+            ...initialState,
+            error:{
+                newTitle:false,
+                newAutor:false,
+                newType: false,
+                newCondition:false,
+            }
         }
     }
 
@@ -29,9 +37,39 @@ class AddBooks extends React.Component{ // AddBooks component
           condition: this.state.newCondition
     
         }
-        if (this.state.newTitle==="" || this.state.newAutor==="" || this.state.newType==="" || this.state.newCondition==="") {
+        if (this.state.newTitle===""){
+            this.setState({
+                error: {
+                    newTitle: this.state.newTitle===""
+                }
+            })
+
+            console.log(this.state)
+
             return 
-        } else {
+        } else if (this.state.newAutor===""){
+            this.setState({
+                error:{
+                    newAutor:this.state.newAutor===""
+                }
+            }) 
+            return
+        } else if (this.state.newType===""){
+            this.setState({
+                error:{
+                    newType:this.state.newType===""
+                }
+            }) 
+            return
+        } else if (this.state.newCondition===""){
+            this.setState({
+                error:{
+                    newCondition:this.state.newType===""
+                }
+            }) 
+            return
+        }
+        else {
         alert(`You added "${this.state.newTitle}`)
         this.setState({
             booksList:[...this.state.booksList, newBook],
@@ -77,41 +115,44 @@ class AddBooks extends React.Component{ // AddBooks component
         })
     }
 
+
+
+
+
      componentDidUpdate() {
         localStorage.setItem("bookslist", JSON.stringify(this.state.booksList));    // local storage updates whenever something changes in this component
       }
 
      render() {
-        const { newTitle, newAutor, newType, newImageUrl, newCondition} = this.state;
+        const { newTitle, newAutor, newType, newImageUrl, newCondition, error} = this.state;
 
         return (
             <div className={styles.wrap}>
                  <form className={styles.form}>
-      <label className={styles.label}>Title:</label>
-      <input className={styles.input} type="text" name="title" placeholder="Insert title name here" value={newTitle} onChange={event => {
+      <label className={styles.label} >Title:</label>
+      <input required className={styles.input} type="text" name="title" placeholder="Insert title name here" value={newTitle} onChange={event => {
         this.handleTitle(event.target.value);
-      }}/>
+      }} className={error.newTitle ? styles.inputError : styles.input}/>
 
-      <label className={styles.label}>Author:</label>
-      <input className={styles.input} type="text" name="autor" placeholder="Insert author name here" value={newAutor} onChange={event => {
-        this.handleAutor(event.target.value);}}/>
+      <label className={styles.label} >Author:</label>
+      <input required className={styles.input} type="text" name="autor" placeholder="Insert author name here" value={newAutor} onChange={event => {
+        this.handleAutor(event.target.value);}} className={error.newAutor ? styles.inputError : styles.input}/>
 
-      <label className={styles.label}>Genre:</label>
-      <input className={styles.input} type="text" name="type" placeholder="Insert genre name here" value={newType} onChange={event => {
-        this.handleType(event.target.value);}}/>
+      <label className={styles.label} >Genre:</label>
+      <input required className={styles.input} type="text" name="type" placeholder="Insert genre name here" value={newType} onChange={event => {
+        this.handleType(event.target.value);}} className={error.newType ? styles.inputError : styles.input}/>
 
       <label className={styles.label}>Cover photo:</label>
       <input className={styles.input} type="text" name="imageUrl" placeholder="URL, ex. http://placekitten.com/140/190 " value={newImageUrl} onChange={event => {
         this.handleImageUrl(event.target.value);}} />
 
       <label className={styles.label}>Condition:</label>
-      <input className={styles.input} type="text" name="condition" placeholder="What is the condition of your book?" value={newCondition} onChange={event => {
-        this.handleCondition(event.target.value);}}/>
+      <input required className={styles.input} type="text" name="condition" placeholder="What is the condition of your book?" value={newCondition} onChange={event => {
+        this.handleCondition(event.target.value);}} className={error.newCondition ? styles.inputError : styles.input}/>
     
     <button className={styles.button} onClick={(e)=>{
                 this.addBook(e);
                 
-                console.log(this.state)
                 
             }}>
                 ADD TO BOOKSWAPP
