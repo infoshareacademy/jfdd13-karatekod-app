bda91f991b87c2036927c0f7c2b95fafe50e9c09
Karolina Michalak <karolina0michalak@gmail.com>
2019-11-19 15:47:01 +0100

    dashboard created

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import styles from '../styles/HomePage.module.css';
+import dashboard1 from '../images/dashboard1.png';
+import dashboard2 from '../images/dashboard2.png';
+import logo from '../images/logo.png'
+
 
 
 const HomePage = () => {
     return (
-        <div>Strona startowa</div>
+        <div className={styles.HomePage}>
+            <div className={styles.headerTop}>
+                <img src={logo} className={styles.logoHome}/><h1> statistics</h1>
+            </div>
+
+            <div className={styles.dashboard1Container}>
+                    <div class={styles.text1}>
+                    This pie chart illustrates number of book types avaliable to swap in %. Fantasy is definitely favorite Book Swappers book genre.
+                    
+                    </div>
+                <img src={dashboard1} className={styles.dashboard1}/>
+                    <div className={styles.dashboard1Stat}>
+                        <label for="square1"><div className={styles.square1}></div> fantasy</label>
+                        <label for="square2"><div className={styles.square2}></div>drama</label>
+                        <label for="square3"><div className={styles.square3}></div>romance</label>
+                        <label for="square4"><div className={styles.square4}></div>thriller</label>
+                        <label for="square5"><div className={styles.square5}></div>guide</label>
+                        <label for="square6"><div className={styles.square6}></div>crime</label>
+                        <label for="square7"><div className={styles.square7}></div>biography</label>
+                        <label for="square8"><div className={styles.square8}></div>other</label>
+                    </div>
+            </div>
+
+
+            <div className={styles.dashboard2Container}>
+                <img src={dashboard2} className={styles.dashboard2}/>
+                <div class={styles.text2}>This graph shows our users number growth from January 2019. As you can see, now our application has over 130 000 
+                    book swappers! Join them now and start book swapping.
+                </div>
+            </div>
+                
+            
+        </div>
     )
 }
 
