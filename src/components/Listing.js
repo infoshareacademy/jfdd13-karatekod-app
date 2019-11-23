6e4e6a2fdd36f809118076241e7ac62b8ecc7ce6
Karol Andracki <andracki@gmail.com>
2019-11-23 18:36:20 +0100

    display number of books but no pagination visible

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -9,9 +9,18 @@ import styles from '../styles/SearchSection.module.css'
      constructor() {
          super()
          this.state = {
+            currentPage: 1,
+            booksPerPage: 5
 
          }
          this.loopListings = this.loopListings.bind(this);
+         this.handleClick= this.handleClick.bind(this)
+     }
+
+     handleClick(event) {
+         this.setState({
+             currentPage: Number(event.target.id)
+         })
      }
 
      loopListings () {
@@ -62,25 +71,49 @@ import styles from '../styles/SearchSection.module.css'
      
 
     render() {
+
+        const {currentPage, booksPerPage} = this.state;
+        const booksList = this.loopListings();
+        const indexOfLastBook = currentPage * booksPerPage;
+        const indexofFirsBook = indexOfLastBook - booksPerPage
+        
+
+
+        const currentBooks = booksList.slice(indexofFirsBook, indexOfLastBook)
+        const renderBooks = currentBooks.map((book, index) => {
+            return <div key = {index}>{book}</div>
+        })
+
+        const pageNumbers = [];
+        for (let i = 1; i <= Math.ceil(booksList.lenght / booksPerPage); i++) {
+            pageNumbers.push(i);
+        }
+
+        const renderPageNumbers = pageNumbers.map(number => {
+            return (
+            <li 
+                key = {number}
+                id = {number}
+                onClick = {this.handleClick}
+                >
+                    {number}
+                </li>);
+        })
+
+
+
+
         return (
             
                 <div>
                 <div className={styles.grid}>
                        
-                       {this.loopListings()}
+                       {renderBooks}
                         
                 </div>
-                <div className = {styles.pagination}>
-                    <ul className={styles.pages}>
-                        <li >Prev</li>
-                        <li className={styles.active}>1</li>
-                        <li>2</li>
-                        <li>3</li>
-                        <li>4</li>
-                        <li>5</li>
-                        <li>Next</li>
-                    </ul>
-                </div>
+                <ul id="page-numbers">
+                    {renderPageNumbers}
+                </ul>
 
             </div>
             
