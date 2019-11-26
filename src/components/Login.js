47c05ca0e1738d9e35b56eba43fae9e2598a729a
Karol Andracki <andracki@gmail.com>
2019-11-26 23:33:32 +0100

    fb auth

---
+++ src/components/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import firebase from 'firebase'
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+
+
+import React, { Component } from 'react'
+
+export class Login extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Login
