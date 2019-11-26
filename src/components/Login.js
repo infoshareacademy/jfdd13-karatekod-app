6422edf0c7da8943f45038cf9fe152ec8dfa4dfb
Karol Andracki <andracki@gmail.com>
2019-11-27 00:28:13 +0100

    web Id and auth domain added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,43 @@ import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
 
 import React, { Component } from 'react'
 
-export class Login extends Component {
+class Login extends Component {
+
+
+    state = {isSignedIn = false}
+    uiConfig = {
+        signedInFlow: "popup",
+        signInOptions: [
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        callbacks: {
+            signInSuccess: () => false
+        }
+    }
+    componentDidMount = () => {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyDMJhPvjpApYZzvwxO2TPJ3Sy3k6vv6GFo',
+            authDomain: 'bookswapp-16613.firebaseapp.com'
+        })
+
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({isSignedIn: !!user})  //if the user is object set to tre if not false
+        })
+    }
+
     render() {
         return (
             <div>
-                
+                {this.state.isSignedIn ? (
+                <div>Is isSignedIn</div>
+                ): (
+                <StyledFirebaseAuth 
+                uiConfig={this.uiConfig}
+                firebaseAuth= {firebase.auth()} 
+                />
+                )}
             </div>
         )
     }
