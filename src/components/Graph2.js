6a9492ed91a55766c4938b0457787bc2c6e5e1c7
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-01 00:57:26 +0100

    removed unnecessary spaces in code

--- src/components/Graph2.js
+++ src/components/Graph2.js
@@ -14,7 +14,7 @@ class Graph2 extends React.Component {
         },
         colors: ['#c2185b', '#e94989', '#f4a4c4', '#ff0080', '#00ccff', '#ff0066', '#ff66ff', '#3399ff']
       },
-      series: [30, 23, 11, 10, 9, 7, 6, 4],
+      series: [],
       booksList:[],
       types: {},
     }
