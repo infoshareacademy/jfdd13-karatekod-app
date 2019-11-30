0ecacedd7d3b03b82be56439c80dcd3cb7ee6fc1
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-01 00:07:56 +0100

    created UserService for firebase fetching

--- src/services/BookService.js
+++ src/services/BookService.js
@@ -1,5 +1,4 @@
 import firebase from "../firebase"
-import { booksList } from "../pages/BooksListPage";
 
 // SERVICE FOR ADDING NEW BOOKS TO BOOKLIST DATABASE ON GOOGLE FIREBASE
 export const addBooksFirebase = (title, autor, type, imageUrl, condition, description) => {
