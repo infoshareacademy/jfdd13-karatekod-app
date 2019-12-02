ae9beb2b68de27640e4d70ec0879cad249852a86
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-02 16:56:50 +0100

    users created fetched as array with dates of creation, will use for graph

--- src/components/Graph1.js
+++ src/components/Graph1.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Chart from "react-apexcharts";
-import {watchUsers, stopUsers} from "../services/UserService"
+import {watchUsersNumber, stopUsers, watchUsers} from "../services/UserService"
+import moment from 'moment'
 
 class Graph1 extends React.Component {
     constructor(props) {
@@ -32,10 +33,30 @@ class Graph1 extends React.Component {
     }
 
     componentDidMount() {
-      watchUsers(users => { // gets booklist from firebase
-        console.log(users)  // creates array with book types 
+      watchUsers(users=>{
+        console.log(users)
+        
+      })
+      watchUsersNumber(users => { // gets booklist from firebase
         this.setState({userNumber:users})
-      });
+        console.log(users)
+      })
+      var date7DaysAgo = moment().subtract(7,'d').format('YYYY-MM-DD');
+      var date6DaysAgo = moment().subtract(6,'d').format('YYYY-MM-DD');
+      var date5DaysAgo = moment().subtract(5,'d').format('YYYY-MM-DD');
+      var date4DaysAgo = moment().subtract(4,'d').format('YYYY-MM-DD');
+      var date3DaysAgo = moment().subtract(3,'d').format('YYYY-MM-DD');
+      var date2DaysAgo = moment().subtract(2,'d').format('YYYY-MM-DD');
+      var date1DaysAgo = moment().subtract(1,'d').format('YYYY-MM-DD');
+      var date0DaysAgo = moment().subtract(0,'d').format('YYYY-MM-DD');
+      console.log(date7DaysAgo)
+      console.log(date6DaysAgo)
+      console.log(date5DaysAgo)
+      console.log(date4DaysAgo)
+      console.log(date3DaysAgo)
+      console.log(date2DaysAgo)
+      console.log(date1DaysAgo)
+      console.log(date0DaysAgo)
     }
 
     componentWillUnmount(){
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -2,20 +2,37 @@ import firebase from "../firebase"
 
 // this service is for watching for new registered users (dashboard statistics)
 
-const prepareUsersCount = response => {
+const prepareUsersCountNumber = response => {
     return Object.keys(response).length     // returns number of users registered in bookswapp
     };
 
 
-export const watchUsers = onSuccess => {    // service for watching users
+export const watchUsersNumber = onSuccess => {    // service for watching users
     return firebase
       .database()
       .ref("/users")
       .on("value", dataSnapshot => {
         const users = dataSnapshot.val();
-        onSuccess(prepareUsersCount(users));
+        onSuccess(prepareUsersCountNumber(users));
       });
   };
+
+  const prepareUsersCreated = response => {
+    return response     // returns number of users registered in bookswapp
+    };
+
+export const watchUsers = onSuccess => {
+  return firebase
+  .database()
+  .ref("/users")
+  .on('value', dataSnapshot => {
+    const users = dataSnapshot.val()
+    const usersProperty = Object.values(users)
+    const usersCreatedAt = usersProperty.map(user=>user.created)
+    onSuccess(prepareUsersCreated(usersCreatedAt))
+  })
+}
+
 
   export const stopUsers = () => {      // stop watching for users (for ex. on component unmounting)
     firebase
