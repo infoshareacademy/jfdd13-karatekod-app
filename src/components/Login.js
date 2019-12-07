ebdd885d746154cabebc1b01d89b28c6e4d5d141
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-07 16:46:38 +0100

    rearranging my profile section

--- src/components/Login.js
+++ src/components/Login.js
@@ -9,7 +9,7 @@ import Favs from '../pages/AdminPage'
 
 
 class Login extends Component {
-    state = {isSignedIn : false}
+    state = { isSignedIn: false }
     uiConfig = {
         signedInFlow: "popup",
         signInOptions: [
@@ -20,23 +20,24 @@ class Login extends Component {
         callbacks: {
             signInSuccess: (auth) => {
                 firebase.database().ref('/users/' + auth.uid).transaction(data => {
-                    console.log('data ',data);
+                    console.log('data ', data);
                     return {
                         name: firebase.auth().currentUser.displayName,
                         created: firebase.auth().currentUser.metadata.creationTime,
                         lastSingIn: firebase.auth().currentUser.metadata.lastSignInTime,
-                        profilePicture: firebase.auth().currentUser.profilePicture}
+                        profilePicture: firebase.auth().currentUser.profilePicture
+                    }
                 })
             }
         }
     }
     componentDidMount = () => {
-        
-        firebase.auth().onAuthStateChanged(user => {
-            this.setState({isSignedIn: !!user}) 
-            console.log("user: ",user)
 
-        }) 
+        firebase.auth().onAuthStateChanged(user => {
+            this.setState({ isSignedIn: !!user })
+            console.log("user: ", user)
+
+        })
 
     }
 
@@ -44,43 +45,48 @@ class Login extends Component {
         return (
             <>
                 {this.state.isSignedIn ? (
-                   <div className={styles.userProfile}>
-                                   <h1 className={styles.h1}>My Profile</h1>
-                        <div style={{clear:"both"}}></div>
-                        <div className={styles.userHeader}>
-                        <div className={styles.userContent}>                          
-                            {/* <img src={firebase.auth().currentUser.photoURL}></img> */}
-                            <ImageUpload/>
-                        </div>
-                        
-                        <Favs />
-                        </div>
+                    <div className={styles.userProfile}>
+                        <div style={{ clear: "both" }}></div>
+                            <div className={styles.userContent}>
+                                {/* <img src={firebase.auth().currentUser.photoURL}></img> */}
+                                <ImageUpload />
+                                <div className={styles.userInfo}>
+                                    <p>Username:</p>
+                                    <p>{firebase.auth().currentUser.displayName}</p>
+                                    <p>Favorite genre:</p>
+                                    <p>{firebase.auth().currentUser.displayName}</p>
+                                    <p>x favorite books</p>
+                                    <p>{firebase.auth().currentUser.displayName}</p>
+                                </div>
+                            </div>
+
+                            <Favs />
                     </div>
 
-                ): (
+                ) : (
 
-              <div className = {styles.loginGrid}>
+                        <div className={styles.loginGrid}>
 
-                    <div className = {styles.logoSection} >
-                       <div className={styles.logo}> <img src={logo}  width="100%"/> </div>
-                      <div className={styles.logoText}>  <img src={logoText}  width="100%"/> </div>
-                    </div>
-                    <div className={styles.loginDesc}>
-                        <p>
-                        Welcome to the world, where your books gain second life. Join the society, where people
-                           share experience through the stories they have red. It's easy like one (Sign in), two
-                           (upload your books), and three (find books to swap)
+                            <div className={styles.logoSection} >
+                                <div className={styles.logo}> <img src={logo} width="100%" /> </div>
+                                <div className={styles.logoText}>  <img src={logoText} width="100%" /> </div>
+                            </div>
+                            <div className={styles.loginDesc}>
+                                <p>
+                                    Welcome to the world, where your books gain second life. Join the society, where people
+                                       share experience through the stories they have red. It's easy like one (Sign in), two
+                                       (upload your books), and three (find books to swap)
                            </p>
-                    </div>
-                    
-                    <div className={styles.loginBox}>                            
-                        <StyledFirebaseAuth 
-                        uiConfig={this.uiConfig}
-                        firebaseAuth= {firebase.auth()} 
-                        />
-                    </div>              
-                </div>
-                )}
+                            </div>
+
+                            <div className={styles.loginBox}>
+                                <StyledFirebaseAuth
+                                    uiConfig={this.uiConfig}
+                                    firebaseAuth={firebase.auth()}
+                                />
+                            </div>
+                        </div>
+                    )}
             </>
         )
     }
