bb20a1353c8650db8a082a72274e44d8632f2a8a
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-07 17:52:23 +0100

    removed hover from profile pic

--- src/components/ImageUpload.js
+++ src/components/ImageUpload.js
@@ -14,18 +14,18 @@ class ImageUpload extends Component {
         this.handleChange = this
             .handleChange
             .bind(this);
-            this.handleUpload = this
+        this.handleUpload = this
             .handleUpload
             .bind(this);
         this.showUpload = this.showUpload.bind(this)
     }
-    showUpload () {
-        const {buttons} = this.state
-       this.setState({
-            
-           buttons: true
-       })
-        
+    showUpload() {
+        const { buttons } = this.state
+        this.setState({
+
+            buttons: true
+        })
+
     }
 
     componentDidMount() {
@@ -33,36 +33,36 @@ class ImageUpload extends Component {
     }
 
     handleChange = e => {
-        if(e.target.files[0]) {
+        if (e.target.files[0]) {
             const image = e.target.files[0];
-            this.setState(() => ({image}));
+            this.setState(() => ({ image }));
         }
     }
     handleUpload = () => {
-        const {image} = this.state;
-        if (image) { 
-        const uploadTask = storage.ref(`images/${image.name}`).put(image);
-        uploadTask.on('state_changed',
-        (snapshot) => {
-            // progress function
-            const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-            this.setState({progress})
-        },
-        (error) => {
-            // error function
-            console.log(error)
-        },
-        () => {
-            // complete function
-            storage.ref('images').child(image.name).getDownloadURL().then(url => {
-                console.log(url);
-                this.setState({url})
-                this.updateProfilePicture(url)
-                this.setState({buttons:false})
-            })
-        });
-    } }
-
+        const { image } = this.state;
+        if (image) {
+            const uploadTask = storage.ref(`images/${image.name}`).put(image);
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    // progress function
+                    const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+                    this.setState({ progress })
+                },
+                (error) => {
+                    // error function
+                    console.log(error)
+                },
+                () => {
+                    // complete function
+                    storage.ref('images').child(image.name).getDownloadURL().then(url => {
+                        console.log(url);
+                        this.setState({ url })
+                        this.updateProfilePicture(url)
+                        this.setState({ buttons: false })
+                    })
+                });
+        }
+    }
     updateProfilePicture = (url) => {
         // 1. check what user are you logged in
         const currentUser = firebase.auth().currentUser
@@ -71,7 +71,6 @@ class ImageUpload extends Component {
         // 2. get the url and update user profile
         firebase.database().ref(`/users/${id}/profilePicture`).set(url)
     }
-
     checkIfUserHasProfilePicture = async () => {
         // 1. get current user id
         const currentUser = firebase.auth().currentUser
@@ -89,33 +88,28 @@ class ImageUpload extends Component {
             })
         }
     }
-
-
     render() {
-        const showProgress = this.state.progress !== 0 && this.state.progress !== 100 
-
+        const showProgress = this.state.progress !== 0 && this.state.progress !== 100
         return (
             <>
-            <div className={styles.profilPictureEdit}>
-                <div className={styles.profilePicture} >  
-                    <img src={this.state.url || "https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg"} alt="Profile pic" height= "200" width= "200" className={styles.userImg}/>
+                <div className={styles.profilPictureEdit}>
+                    <div className={styles.profilePicture} >
+                        <img src={this.state.url || "https://immedilet-invest.com/wp-content/uploads/2016/01/user-placeholder.jpg"} alt="Profile pic" height="200" width="200" className={styles.userImg} />
+                    </div>
+                    {/* {showProgress && <progress value={this.state.progress} max="100"/>} */}
+
                 </div>
-                {/* {showProgress && <progress value={this.state.progress} max="100"/>} */}
-                
-            <div className={styles.backOfProfilePic} onClick={this.showUpload}>Click here to upload image</div>
-             </div>
-             <div className={styles.loadPicSec} >
-                    
-             { (this.state.buttons) ? (
-             <div className={styles.uploadButtons}>
-                 <label for="file" className={styles.inputFileLabel}>Choose new Profile Picture</label>
-             <input className={styles.inputFileHidden} type="file" onChange={this.handleChange} accept="image/*" style={{width:'100%'}} name="file" id="file"/> 
-             <button className={styles.uploadButton} onClick={this.handleUpload}>Upload</button>
-             </div>): (null)
-             }   
-             
-         </div>
-         </>
+                <div className={styles.loadPicSec} >
+                    {(this.state.buttons) ? (
+                        <div className={styles.uploadButtons}>
+                            <label for="file" className={styles.inputFileLabel}>Choose new Profile Picture</label>
+                            <input className={styles.inputFileHidden} type="file" onChange={this.handleChange} accept="image/*" style={{ width: '100%' }} name="file" id="file" />
+                            <button className={styles.uploadButton} onClick={this.handleUpload}>Upload</button>
+                        </div>) : (null)
+                    }
+
+                </div>
+            </>
         )
     }
 }
