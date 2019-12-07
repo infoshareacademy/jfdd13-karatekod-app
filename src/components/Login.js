1da1ff9209bfb26b6b2c6d489c3caab4b13326f6
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-07 21:24:55 +0100

    profile name under profile pic

--- src/components/Login.js
+++ src/components/Login.js
@@ -45,7 +45,6 @@ class Login extends Component {
             <>
                 {this.state.isSignedIn ? (
                     <div className={styles.userProfile}>
-                        <h1>Hello, {firebase.auth().currentUser.displayName}!</h1>
                         <div style={{ clear: "both" }}></div>
                             <div className={styles.userContent}>
                                 {/* <img src={firebase.auth().currentUser.photoURL}></img> */}
