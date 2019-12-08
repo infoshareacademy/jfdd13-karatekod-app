6c3d669346e9abc489da53b69c8d147b247797cd
Karolina Michalak <karolina0michalak@gmail.com>
2019-12-08 15:51:38 +0100

    white logo on header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -2,12 +2,35 @@ import React from 'react';
 import styles from '../styles/Header.module.css';
 import { Route, Switch } from 'react-router-dom'
 import photo4 from '../images/photo4.jpg';
+import logoWhite from '../images/logoWhite.png'
+
 
 const Header = () => {
     return (
         <div className={styles.header}>         
             <Switch>
-                <Route path="/"  render={() =>( <img className={styles.headerImg} src={photo4} alt="header" />)} />
+                <Route path="/"  
+                render={() =>(
+                    
+                <div>
+                <img className={styles.headerImg} src={photo4} alt="header"/>
+                <img style={{
+                    position: 'absolute',
+                    width: '320px',
+                    height: '50px',
+                    top: 0,
+                    left: 0,
+                    zIndex: 1000,
+                    marginLeft: '100px',
+                    marginTop: '40px'
+                }} src={logoWhite}/>
+                    </div>
+                               
+
+                                
+                
+                
+                )} />
             </Switch>
      </div>
     )
