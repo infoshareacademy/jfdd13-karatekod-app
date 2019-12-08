4d2f0fdfd0271f4c3f8b846389bfe439d15e1f43
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-12-08 16:13:09 +0100

    removed console logs and empty spaces

--- src/components/Graph2.js
+++ src/components/Graph2.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Chart from "react-apexcharts";
 import { watchBooks, stopBooks } from "../services/BookService"
-
 class Graph2 extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       options: {
         labels: ["fantasy", "drama", "romance", "thriller", "guides", "crime", "biography", "other"],
@@ -15,45 +13,42 @@ class Graph2 extends React.Component {
         colors: ['#c2185b', '#e94989', '#f4a4c4', '#ff0080', '#00ccff', '#ff0066', '#ff66ff', '#3399ff']
       },
       series: [],
-      booksList:[],
+      booksList: [],
       types: {},
     }
   }
-
   componentDidMount() {
-    watchBooks(booksList => { // gets booklist from firebase
-      let types = booksList.map(x=>x.type)  // creates array with book types
-      let myTypes = types.reduce((result,next)=>{ // counts the number each type occured and puts it into object myTypes {fantasy: 42, drama: 15... etc}
-        result[next]=(result[next] || 0) +1;
+    watchBooks(booksList => {
+      let types = booksList.map(x => x.type)
+      let myTypes = types.reduce((result, next) => {
+        result[next] = (result[next] || 0) + 1;
         return result;
-        }, {})
-        this.setState({types:myTypes})  // puts
-        this.setState({names:[Object.keys(myTypes)]})
-        this.setState({counts:[Object.values(myTypes)]})
-        this.setState({options:{
+      }, {})
+      this.setState({ types: myTypes })
+      this.setState({ names: [Object.keys(myTypes)] })
+      this.setState({ counts: [Object.values(myTypes)] })
+      this.setState({
+        options: {
           fill: {
             colors: ['#c2185b', '#e94989', '#f4a4c4', '#ff0080', '#00ccff', '#ff0066', '#ff66ff', '#3399ff']
           },
           colors: ['#c2185b', '#e94989', '#f4a4c4', '#ff0080', '#00ccff', '#ff0066', '#ff66ff', '#3399ff'],
         }
       })
-        this.setState({series:Object.values(myTypes)})       
+      this.setState({ series: Object.values(myTypes) })
     });
   }
-
-  componentWillUnmount(){
+  componentWillUnmount() {
     stopBooks();
   }
-
   render() {
     return (
-      <div style={{width:'100%'}}>
+      <div style={{ width: '100%' }}>
         <Chart options={this.state.options} series={this.state.series} colors={this.state.colors} type="donut" width="100%" />
       </div>
     );
   }
 }
-
 export default Graph2;
 
 
